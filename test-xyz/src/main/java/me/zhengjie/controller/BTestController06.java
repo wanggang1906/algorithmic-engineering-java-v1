8a06d2f76e54e5669670d17cb98b802d96dd5ba6
wanggang1906 <1294901911@qq.com>
2020-05-30 10:09:23 +0800

    crud详解

--- test-xyz/src/main/java/me/zhengjie/controller/BTestController06.java
+++ test-xyz/src/main/java/me/zhengjie/controller/BTestController06.java
@@ -1,10 +1,8 @@
 package me.zhengjie.controller;
 
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import me.zhengjie.annotation.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,11 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor // 所需要参数的构造函数
 @Api(tags = "自定义：链路测试")
-@RequestMapping("/api/con/self0302")
-public class BTestController0302 {
+@RequestMapping("/api/con/self06")
+public class BTestController06 {
 
-
-    @Log("导出部门数据")
     @ApiOperation("查询部门")
     @GetMapping
     @PreAuthorize("@el.check('user:list','dept:list')")
@@ -30,5 +26,4 @@ public class BTestController0302 {
         String result = "来自后台";
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
 }
