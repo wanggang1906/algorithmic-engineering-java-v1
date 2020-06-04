f592f13aa3ea586a0fae4b448393b63bf39142f5
wanggang1906 <1294901911@qq.com>
2020-06-04 17:40:26 +0800

    crud表格数据请求及返回

--- test-con/src/main/java/me/zhengjie/controller/BTestController03.java
+++ test-con/src/main/java/me/zhengjie/controller/BTestController03.java
@@ -11,6 +11,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor // 所需要参数的构造函数
 @Api(tags = "自定义：链路测试")
@@ -21,26 +25,86 @@ import org.springframework.web.multipart.MultipartFile;
 public class BTestController03 {
 
 
-    @ApiOperation("查询部门")
+    @ApiOperation("测试请求数据")
     @GetMapping
     @PreAuthorize("@el.check('user:list','dept:list')")
     public ResponseEntity<Object> query() throws Exception {
+        // crud获取数据
+        HashMap<String,String> tableMap1 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
+        HashMap<String,String> tableMap3 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
+        HashMap<String,String> tableMap4 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
 
-        // DeptQueryCriteria criteria
-        //String  name = criteria.getName();
-        String result = "来自后台";
+/*
+        // guava的方式
+        Map<String, Integer> left = ImmutableMap.of("a", 1, "b", 2, "c", 3);
+        //或者
+        Map<String, String> test = ImmutableMap.<String, String>builder()
+                .put("k1", "v1")
+                .put("k2", "v2")
+        ...
+        .build();*/
+
+        //HashMap<String,String> tableMap2 = new HashMap<>();
+        List<HashMap<String,String>> result = new ArrayList<HashMap<String, String>>();
+        result.add(tableMap1);
+        result.add(tableMap3);
+        result.add(tableMap4);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+
+
+    @ApiOperation("测试请求数据-带参数")
+    @GetMapping("/param")
+    @PreAuthorize("@el.check('user:list','dept:list')")
+    public ResponseEntity<Object> queryOfParam(@RequestParam("id")String id) {
+        // crud获取数据
+        HashMap<String,String> tableMap1 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
+        HashMap<String,String> tableMap3 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
+        HashMap<String,String> tableMap4 = new HashMap<String,String>(){
+            { put("id","2"); }
+            { put("name","name"); }
+        };
+
+        System.out.println(id);
+
+        //HashMap<String,String> tableMap2 = new HashMap<>();
+        List<HashMap<String,String>> result = new ArrayList<HashMap<String, String>>();
+        result.add(tableMap1);
+        result.add(tableMap3);
+        result.add(tableMap4);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
 
     @ApiOperation("图片上传")
     @PostMapping("/upload")
     @PreAuthorize("@el.check('user:list','dept:list')")
-    public ResponseEntity<Object> uploadFile(@RequestParam MultipartFile pictureFile){
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile pictureFile){
         // 图片文件上传
 
         // 把有效的图片重命名，存储，返回存储路径和文件名
         if(!pictureFile.isEmpty()){
-            // savePictureFile
+            System.out.println("土拍你");
+            // 存储图片
         }
         return new ResponseEntity<>("ok",HttpStatus.OK);
     }
